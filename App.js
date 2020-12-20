9a4fb437377b156db5994e5784ec587f72ddf61a
Telmo Gonçalves <telmogferreira@gmail.com>
2020-12-19 21:59:41 -0300

    alteracao no state do botao para abrir novo biscoito

---
+++ App.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity
+    } from 'react-native';
+
+
+class App extends Component {
+
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            textoFrase: 'Quebre o biscoito e veja sua sorte',
+            img: require('./src/biscoito.png'),
+            txtBotton: 'Quebrar Biscoito',
+        };
+
+        this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+        
+
+        this.frases = [
+
+            'Siga os bons e aprenda com eles.', 
+            'O bom-senso vale mais do que muito conhecimento.', 
+            'O riso é a menor distância entre duas pessoas.', 
+            'Deixe de lado as preocupações e seja feliz.',
+            'Realize o óbvio, pense no improvável e conquiste o impossível.',
+            'Acredite em milagres, mas não dependa deles.',
+            'A maior barreira para o sucesso é o medo do fracasso.'
+        ];
+    }
+
+    quebrarBiscoito(){
+
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+            
+            if(this.state.txtBotton == 'Quebrar Biscoito'){
+                
+                this.setState({
+                txtBotton: 'Novo Biscoito',
+                textoFrase: this.frases[numeroAleatorio],
+                img: require('./src/biscoitoAberto.png')
+
+            })
+
+            }else{
+
+                this.setState({
+                    textoFrase: 'Quebre o biscoito e veja sua sorte',
+                    img: require('./src/biscoito.png'),
+                    txtBotton: 'Quebrar Biscoito'
+                })
+            }
+            ;
+        
+
+    };
+
+    
+    
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    style={styles.img}
+                    source={this.state.img}                   
+                />
+
+                <Text style={styles.txt}>{this.state.textoFrase}</Text> 
+
+                <TouchableOpacity 
+                style={styles.btn} 
+                onPress={this.quebrarBiscoito}>
+
+                    <View style={styles.btnArea}>
+
+                        <Text style={styles.btntext}>
+                            {this.state.txtBotton}
+                        </Text>
+
+                    </View>
+                </TouchableOpacity>
+
+            </View>
+        
+    )};
+
+}    
+
+
+
+
+
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"  
+    },
+
+    txt:{
+        fontSize: 20,
+        color: "#dd7d22",
+        fontStyle: "italic",
+        margin:30,
+        textAlign:"center"
+    },
+
+    img: {
+        width: 250,
+        height: 250,
+    },
+
+    btn:{
+        width: 250,
+        height: 50,
+        borderWidth:2,
+        borderColor:"#dd7d22",
+        borderRadius:25,
+        margin:10
+        
+
+    },
+
+    btnArea:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    btntext:{
+
+        color: "#dd7d22",
+        textAlign:"center",       
+        fontWeight:"bold",
+        fontSize:18
+        
+      
+
+
+    }
+})
+
+export default App;
